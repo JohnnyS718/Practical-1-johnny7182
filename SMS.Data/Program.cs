52be8332fb357c47a7795234cdb18a0ce1238286
JohnnyS718 <105826124+JohnnyS718@users.noreply.github.com>
2023-06-13 12:05:33 +0000

    rrfrf

--- SMS.Data/Program.cs
+++ SMS.Data/Program.cs
@@ -3,45 +3,46 @@ using SMS.Data.Extensions;          // where our List extension method (ToPretty
 using SMS.Data.Repository;          // where our data repository is defined
 
 // call relevant methods here to test
-Question1();
+//Question1();
 Question3();
-Question4();
-Question5();
-// optional question
+//Question4();
+//Question5();
+//optional question
 //Question6();
       
-
 // Question 1 - Query Int List
 void Question1()
 {
     Console.WriteLine("\nQuestion 1");
     
     //q1.1 - create integer list (data) and intialise with specified values
-    var data = new List<int> { 23,4,7,1,12,45,19,7 };
+    var data = new List<int> {23, 4, 7, 1, 12, 45, 19, 7};
     
     // q1.2
     Console.Write("\nq1.2: ");
-    Console.WriteLine(data.ToPrettyString());
+    Console.Write("[ ");
+    data.ForEach(e => Console.Write($"{e}, "));
+    Console.Write("]");
     
     // q1.3
     Console.Write("\nq1.3: ");
-    var over15 = data.Where(e => e > 15).ToList();
+    var over15 = data.Where(n => n > 15).ToList();
     over15.Print();
     
     // q1.4
     Console.Write("\nq1.4: ");
-    var between5and15 = data.Where(e => e > 5 && e < 15).ToList();
-    between5and15.Print();
+    var over5less15 = data.Where(n => n > 5 && n < 15).ToList();
+    over5less15.Print();
 
     // q1.5
     Console.Write("\nq1.5: ");
-    var allunique = data.Distinct().ToList();
-    allunique.Print();
+    var distinctover5less15 = data.Where(n => n > 5 && n < 15).Distinct().ToList();
+    distinctover5less15.Print();
     
     // q1.6       
     Console.Write("\nq1.6: ");
-    var allinorder = data.OrderBy(e => e).ToList();
-    allinorder.Print();
+    var listSorted = data.OrderBy(e => e).ToList();
+    listSorted.Print();
 }
 
 // Q2 - Seed the Database 
@@ -51,11 +52,11 @@ void SeedDatabase(DataContext db)
     db.Initialise();
     
     // create students
-    var s1 = new Student {Name = "Homer", Age = 44};
-    var s2 = new Student {Name = "Marge", Age = 38};
-    var s3 = new Student {Name = "Bart", Age = 12};
-    var s4 = new Student {Name = "Lisa", Age = 10};
-    var s5 = new Student {Name = "Burns", Age = 80};
+    var s1 = new Student{Id = 1, Name = "Homer", Age = 44};
+    var s2 = new Student{Id = 2, Name = "Marge", Age = 38};
+    var s3 = new Student{Id = 3, Name = "Bart", Age = 12};
+    var s4 = new Student{Id = 4, Name = "Lisa", Age = 10};
+    var s5 = new Student{Id = 5, Name = "Burns", Age = 80};
 
     // add students to database and save changes
     db.Students.AddRange(s1,s2,s3,s4,s5);
@@ -71,40 +72,39 @@ void Question3()
     // create database context and call seed database
     var db = new DataContext();
     SeedDatabase(db);
-    
-    // q3.1 - print the students in the database        
-    Console.WriteLine("\nq3.1: ");
+  
+    // q3.1 - print the students in the database
     var all = db.Students.ToList();
     all.Print();
    
     //q3.2 - Find and print the student with the Id 1
-    Console.WriteLine("\nq3.2");
-    var s = db.Students.FirstOrDefault(s => s.Id == 1);
-    Console.WriteLine(s);
+    var first = db.Students.FirstOrDefault(e => e.Id == 1);
+    Console.WriteLine(first);
       
     // q3.3 - print students with Age > 20
     Console.WriteLine("\nq3.3: ");
-    var over20 = db.Students.Where(s => s.Age > 20).ToList();
-    over20.Print();  
-      
+    var ageOver20 = db.Students.Where(s => s.Age > 20).ToList();
+    ageOver20.Print();
+
     // q3.4 Select and print the Name and Age of all students (see custom projection in the notes)
     Console.WriteLine("\nq3.4: ");
-    var q4 = db.Students.Select(s => new {s.Name, s.Age}).ToList();
-    q4.Print();  
+    var names = db.Students.Select(s => new {s.Name, s.Age}).ToList();
+    names.Print();
 
-    // q3.5 - print the Age of the first student over 20. Use FirstOrDefault
-    //        deal with scenario where a student may not be found before attempting to print the age
+    // q3.5 - print the Age of the first student over 20. Use                  FirstOrDefault
+    // deal with scenario where a student may not be found before              attempting to print the age
     Console.WriteLine("\nq3.5");
-    //var sover20 = db.Students.OrderBy.(s => s.Age )FirstOrDefault.(s => s.Age > 20);
-    //if (sover20 is not null)
-    //{
-    //  Console.WriteLine($"Age of first student over 20 is: {sover20.Age}");
-    //}else
-    //  {
-    //    Console.WriteLine("Student does not exist");
-    //  }
+    var age = 20;
     
-    
+    var studentsOver20 = db.Students.OrderBy(s => s.Age).FirstOrDefault(s => s.Age > age);
+    if(studentsOver20 is not null)
+    {
+      Console.WriteLine(studentsOver20);
+    }
+    else
+    {
+      Console.WriteLine($"There are no students over the age of {age}");
+    }
 }
 
 // Q4 - More advanced queries
@@ -113,23 +113,18 @@ void Question4()
     Console.WriteLine("\nQuestion 4");
     
     // create database context and seed database
-    var db = new DataContext();
-    SeedDatabase(db);
+
 
     //4.1. print the students by descending age
     Console.WriteLine("\nq4.1: ");
-    var agedesc = db.Students.OrderByDescending(e => e.Age).ToList();
-    agedesc.Print();
+
     
     //4.2. print count of number of students who are over 18 (should print 3)
     Console.WriteLine("\nq4.3");
-    var over18 = db.Students.Count(s => s.Age > 18);
-    Console.WriteLine($"Number over 18 is: {over18}");
+
 
     //4.2. print Age of oldest Student (should print 80)
     Console.WriteLine("\nq4.4");
-    var oldest = db.Students.OrderByDescending(e => e.Age).FirstOrDefault();
-    Console.WriteLine(oldest);
    
 }
 
@@ -139,25 +134,14 @@ void Question5()
     Console.WriteLine("\nQuestion 5");
             
     // create database context and seed database          
-    var db = new DataContext();
-    SeedDatabase(db);
     
     //5.1. Update Age of Student with Id 3 to 13
     Console.WriteLine("5.1 - updating Age of Student 3 (Bart)");
-    var bart = db.Students.Find(3);
-    Console.WriteLine($"Bart's old age: {bart.Age}");
-    bart.Age = 13;
-    db.SaveChanges();
-    Console.WriteLine($"Bart's new age is: {bart.Age}");
+
    
     //5.2. Update student with name "Burns" to "Mr Burns"
     Console.WriteLine("\nq5.2 - updating Burns name");
-    var burnschange = db.Students.Where(e => e.Name == "Burns");
-    Console.WriteLine($"Name before change: {burnschange.Name}");
-    burnschange.Name = "Mr Burns";
-    db.SaveChanges();  
-    burnschange = db.Students.Where(e => e.Name == "Mr Burns");
-    Console.WriteLine($"Name after change: {burnschange.Name}");    
+
     
     //5.3. Delete Marge
     Console.WriteLine("\nq5.3 - delete first student with name Marge");
